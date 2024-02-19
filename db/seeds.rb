b2a477d6d6154f4e0955571598a528b4e16f4f22
contactpankaj <contact_pankaj@tamu.edu>
2024-02-19 14:56:22 -0600

    movie db created

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,30 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+more_movies = [
+  {:title => 'My Neighbor Totoro', :rating => 'G',
+    :release_date => '16-Apr-1988'},
+  {:title => 'Green Book', :rating => 'PG-13',
+    :release_date => '16-Nov-2018'},
+  {:title => 'Parasite', :rating => 'R',
+    :release_date => '30-May-2019'},
+  {:title => 'Nomadland', :rating => 'R',
+    :release_date => '19-Feb-2021'},
+  {:title => 'CODA', :rating => 'PG-13',
+    :release_date => '13-Aug-2021'},
+  # New movies
+  {:title => 'The Shawshank Redemption', :rating => 'R',
+    :release_date => '14-Oct-1994'},
+  {:title => 'Inception', :rating => 'PG-13',
+    :release_date => '16-Jul-2010'},
+  {:title => 'The Godfather', :rating => 'R',
+    :release_date => '24-Mar-1972'},
+  {:title => 'Pulp Fiction', :rating => 'R',
+    :release_date => '14-Oct-1994'}
+]
+
+more_movies.each do |movie|
+  Movie.create!(movie)
+end
+
